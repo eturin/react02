2f9dc0044a717fcfc8be8346f388d6259c8bf3d7
Stas <eturin@gmail.com>
2020-06-20 09:57:59 +0300

    04

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -2,7 +2,7 @@ import React from "react";
 //import css from './Settings.module.css'
 import EditProfContainer from "../ProfileContent/EditProf/EditProfContainer";
 
-const Settings = React.memo(() => {
+const Settings = React.memo<{}>(() => {
     return (
         <div>
             <h1>Настройки</h1>
